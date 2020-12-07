b34990bf5f52bc8b887106a1553541eb1e3d4b35
Cao Mai <caomai@rocketmail.com>
2020-12-06 21:53:11 -0800

    [add] left and iright rotate methods

--- Code/avltree.py
+++ Code/avltree.py
@@ -20,25 +20,30 @@ class AVLTree(object):
         else:
             root.right = self.insert_node(root.right, value)
 
+        # updating root height of current node
         root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
 
         # get balance factor and balance tree
         balance_factor = self.get_balance_factor(root)
 
+        # if balance_factor difference is greater than 1 then need to rotate
         if balance_factor > 1:
             if value < root.left.value:
                 return self.right_rotate(root)
             else:
+                # you have to rotate twice 
                 root.left = self.left_rotate(root.left)
                 return self.right_rotate(root)
         if balance_factor < -1:
             if value > root.right.value:
                 return self.left_rotate(root)
             else:
+                # you have to rotate twice 
                 root.right = self.right_rotate(root.right)
                 return self.left_rotate(root)
+        
+        return root
 
-    
     def get_height(self, root):
         if not root:
             return 0
@@ -48,6 +53,38 @@ class AVLTree(object):
         if not root:
             return 0
         return self.get_height(root.left) - self.get_height(root.right)
+
+    # z could be root?
+    # left rotation
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        # swapping
+        y.left = z
+        z.right = T2
+
+        # updating the node's heights
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        # return new root node
+        return y
+    
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return y
+
+
+
 
 
 
